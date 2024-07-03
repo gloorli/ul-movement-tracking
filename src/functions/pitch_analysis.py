57f2cf1472723a143ed4e53b2e1642b60c8377a8
gloorli <linus.gloor@inf.ethz.ch>
2024-07-03 13:48:45 +0800

    feat: add Balasubramanian pitch calculation method

--- src/functions/pitch_analysis.py
+++ src/functions/pitch_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import signal
 
 def read_pitch_from_csv(file_path):
     """
@@ -15,3 +16,14 @@ def plot_pitch_data(pitch_data):
     Plot pitch data.
     """
     pitch_data.plot(x='loggingTime(txt)', y='motionPitch(deg)', title='Pitch data', figsize=(12, 6))
+
+def estimate_pitch(accl: np.array, farm_inx: int, nwin: int) -> np.array:
+    """
+    Estimates the pitch angle of the forearm from the accelerometer data.
+    Adapted from Balasubramanian 2023
+    """
+    # Moving averaging using the causal filter
+    acclf = signal.lfilter(np.ones(nwin) / nwin, 1, accl, axis=0) if nwin > 1 else accl
+    # Compute the norm of the acceleration vector
+    acclfn = acclf / np.linalg.norm(acclf, axis=1, keepdims=True)
+    return -np.rad2deg(np.arccos(acclfn[:, farm_inx])) + 90
