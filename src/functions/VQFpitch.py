94805644110b2a61d0a992527f0b38a0ad027bed
gloorli <linus.gloor@inf.ethz.ch>
2024-08-01 17:46:02 +0800

    feat: add elevation calculation method from Leuenberger 2017

--- src/functions/VQFpitch.py
+++ src/functions/VQFpitch.py
@@ -19,26 +19,49 @@ class IMUPitchCalculator:
         Convert quaternions from scalar-first to scalar-last convention.
         """
         return np.roll(quaternions, -1, axis=1)
-        
-    def calculate_pitch(self):
-        # Calculate quaternions using vqf.offlineVQF
+    
+    def calculate_vqf_quaternions(self):
+        """
+        Calculates the VQF quaternions by using gyro and accelerometer data. The quaternions are then converted from the scalar-first convention to the scalar-last convention.
+
+        Returns:
+            numpy.ndarray: The quaternions in the scalar-last convention.
+        """
         quaternions = vqf.offlineVQF(self.gyro, self.accel, None, self.Ts)
         quaternions_scalar_last = self.scalar_first_to_scalar_last(quaternions['quat6D'])
+        return quaternions_scalar_last
+
+    def calculate_pitch(self):
+        # Calculate quaternions using vqf.offlineVQF
+        quaternions_scalar_last = self.calculate_vqf_quaternions()
         # Convert quaternions to Euler angles
         r = R.from_quat(quaternions_scalar_last)
-        euler_angles = r.as_euler('yxz', degrees=True) #TODO think about convention
+        euler_angles = r.as_euler('yxz', degrees=True)
         
         # Extract pitch from the Euler angles
         self.pitch = euler_angles[:, 1]
         return self.pitch
     
+    def calculate_elevation(self):
+        # Calculate quaternions using vqf.offlineVQF
+        quaternions_scalar_last = self.calculate_vqf_quaternions()
+        # Convert quaternions to Elevation [Leuenberger, 2017]
+        r = R.from_quat(quaternions_scalar_last)
+        rotation_matrix = r.as_matrix()
+        a_s = np.array([0, 1, 0], dtype=float)
+        #a_e = np.matmul(rotation_matrix.T, a_s.T)
+        a_e = np.array([rotation_matrix_i @ a_s.T for rotation_matrix_i in rotation_matrix])
+        self.elevation = np.degrees(np.array([np.arctan2(a_e_i[2], np.sqrt(a_e_i[1]**2 + a_e_i[0]**2), ) for a_e_i in a_e]))
+        return self.elevation
+    
     def cut_recording(self, start_index, end_index):
         # Cut the pitch data
         self.pitch = self.pitch[start_index:end_index]
+        self.elevation = self.elevation[start_index:end_index]
     
-    def plot_pitch(self):
+    def plot_pitch(self, data):
         # Create a DataFrame for pitch data
-        pitch_data = pd.DataFrame({'loggingTime(txt)': np.arange(len(self.pitch)) * self.Ts, 'motionPitch(deg)': self.pitch})
+        pitch_data = pd.DataFrame({'loggingTime(txt)': np.arange(len(data)) * self.Ts, 'motionPitch(deg)': data})
         
         # Plot pitch data
         pitch_data.plot(x='loggingTime(txt)', y='motionPitch(deg)', title='Pitch data', figsize=(12, 6))
