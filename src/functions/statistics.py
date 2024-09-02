efde34153c5729f18bbb0821bc0bae975bf833cc
gloorli <linus.gloor@inf.ethz.ch>
2024-09-02 20:37:25 +0800

    refactor: don't plot log regression, don't plot dataset distribution and add r squared value to legend and p-value to spearman/pearson

--- src/functions/statistics.py
+++ src/functions/statistics.py
@@ -24,12 +24,7 @@ class RegressionModel:
         poly_features = PolynomialFeatures(degree)
         poly_model = make_pipeline(poly_features, LinearRegression())
         poly_model.fit(self.x, self.y)
-        self.poly_models[degree] = poly_model
-
-    def fit_logarithmic_regression(self):
-        x_log = np.log(self.x)
-        self.log_model = LinearRegression()
-        self.log_model.fit(x_log, self.y)
+        self.poly_models[degree] = poly_model  
 
     def predict_linear(self, x):
         if self.linear_model is None:
@@ -42,40 +37,14 @@ class RegressionModel:
             raise ValueError(f"Polynomial model of degree {degree} is not fitted yet.")
         x = np.array(x).reshape(-1, 1)
         return self.poly_models[degree].predict(x)
-    
-    def predict_logarithmic(self, x):
-        if self.log_model is None:
-            raise ValueError("Logarithmic model is not fitted yet.")
-        x_log = np.log(np.array(x).reshape(-1, 1))
-        return self.log_model.predict(x_log)
-    
-    def check_distribution(self):
-        plt.figure(figsize=(12, 5))
-
-        # Distribution of x
-        plt.subplot(1, 2, 1)
-        plt.hist(self.x, bins=self.x.shape[0], edgecolor='k', alpha=0.7)
-        plt.title('Distribution of x')
-        plt.xlabel('x')
-        plt.ylabel('Frequency')
-
-        # Distribution of y
-        plt.subplot(1, 2, 2)
-        plt.hist(self.y, bins=66, edgecolor='k', alpha=0.7)
-        plt.title('Distribution of y')
-        plt.xlabel('y')
-        plt.ylabel('Frequency')
-
-        plt.tight_layout()
-        plt.show()
 
     def pearson_correlation(self):
-        corr, _ = pearsonr(self.x.flatten(), self.y)
-        return corr
+        corr, pvalue = pearsonr(self.x.flatten(), self.y)
+        return corr, pvalue
 
     def spearman_correlation(self):
-        corr, _ = spearmanr(self.x.flatten(), self.y)
-        return corr
+        corr, pvalue = spearmanr(self.x.flatten(), self.y)
+        return corr, pvalue
 
     def plot_regressions(self, title='Regression Plots', xlabel='x', ylabel='y'):
         colors = thesis_style.get_thesis_colours()
@@ -99,18 +68,14 @@ class RegressionModel:
         if self.linear_model is not None:
             x_range = np.linspace(self.x.min(), self.x.max(), 500).reshape(-1, 1)
             y_linear_pred = self.linear_model.predict(x_range)
-            plt.plot(x_range, y_linear_pred, color='red', label='Linear Regression')
+            r_squared = self.linear_model.score(self.x, self.y)
+            plt.plot(x_range, y_linear_pred, color='red', label=f'Linear Regression (R-squared: {r_squared:.2f})')
 
         # Plot polynomial regressions
         for degree, poly_model in self.poly_models.items():
             y_poly_pred = poly_model.predict(x_range)
-            plt.plot(x_range, y_poly_pred, label=f'Polynomial Regression (degree {degree})', color=colors['light_blue'])
-
-        # Plot logarithmic regression
-        if self.log_model is not None:
-            x_range_log = np.linspace(self.x.min(), self.x.max(), 500).reshape(-1, 1)
-            y_log_pred = self.log_model.predict(np.log(x_range_log))
-            plt.plot(x_range_log, y_log_pred, color='green', label='Logarithmic Regression')
+            r_squared = poly_model.score(self.x, self.y)
+            plt.plot(x_range, y_poly_pred, label=f'Polynomial Regression (degree {degree}) (R-squared: {r_squared:.2f})', color=colors['light_blue'])
 
         if ylabel == 'Count Threshold':
             plt.axhline(y=0.0, color=colors['black_grey'], linestyle='--', label='Conventional threshold')
@@ -151,17 +116,14 @@ def check_regression(x, y, std, x_label='x', y_label='y', title='Regression Anal
 
     # Fit polynomial regression of degree 2
     model.fit_polynomial_regression(2)
-
-    # Fit logarithmic regression
-    model.fit_logarithmic_regression()
         
     # Calculate Pearson correlation
-    pearson_corr = model.pearson_correlation()
-    print("Pearson Correlation:", pearson_corr)
+    pearson_corr, pearson_pvalue = model.pearson_correlation()
+    print("Pearson Correlation:", pearson_corr, "p-value:", pearson_pvalue)
 
     # Calculate Spearman correlation
-    spearman_corr = model.spearman_correlation()
-    print("Spearman Correlation:", spearman_corr)
+    spearman_corr, spearman_pvalue = model.spearman_correlation()
+    print("Spearman Correlation:", spearman_corr, "p-value:", spearman_pvalue)
 
     # Plot regressions
     model.plot_regressions(xlabel=x_label, ylabel=y_label, title=title)
